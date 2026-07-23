d30b77be999f999b2ee7997d422283ed175eadd7
lestergilang-jpg <lestergilang@gmail.com>
2026-07-23 15:59:27 +0700

    fix(api): increase default database connection pool max to 100 to prevent timeout

--- apps/api/src/configs/database.config.ts
+++ apps/api/src/configs/database.config.ts
@@ -7,7 +7,7 @@ export function DatabaseConfig() {
       || 'postgres://postgres:postgres@localhost:5432/volvecapital',
       pool: {
         min: Number.parseInt(process.env.DATABASE_POOL_MIN || '0'),
-        max: Number.parseInt(process.env.DATABASE_POOL_MAX || '10'),
+        max: Number.parseInt(process.env.DATABASE_POOL_MAX || '100'),
         acquire: Number.parseInt(process.env.DATABASE_POOL_AQUIRE || '30000'),
         idle: Number.parseInt(process.env.DATABASE_POOL_IDLE || '10000'),
         evict: Number.parseInt(process.env.DATABASE_POOL_EVICT || '1000'),
